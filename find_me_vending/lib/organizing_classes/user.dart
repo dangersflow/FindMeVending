5e3fde844d4ff6adeaf4525c9db2eb603c162c53
Lauryn Brough <lauryn.brough@hotmail.com>
2020-04-02 14:29:58 -0500

    updating users to show name on menu. Not fully implemented to do delays in futures. Shows on refresh, but not immediately on login.

--- find_me_vending/lib/organizing_classes/user.dart
+++ find_me_vending/lib/organizing_classes/user.dart
@@ -1,10 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 class User {
   String uid;
   String name;
   int points;
   List favorites;
+  DocumentReference doc;
+  FirebaseUser user;
 
-  User(this.uid, this.name, this.points, this.favorites);
+  User({this.uid, this.name, this.points, this.favorites, this.doc, this.user}) {
+    if(doc != null) {
+      processDoc();
+    }
+  }
+
+  Future<void> processDoc() async{
+    if(doc == null) {
+      return;
+    }
+
+    DocumentSnapshot snap = await doc.get();
+    this.favorites = snap.data['favorites'];
+    this.uid = snap.data['uid'];
+    this.points = snap.data['points'];
+    this.name = snap.data['name'];
+  }
 }
 
-User testUser = User("YwPkeqpWCMROTHtwTzu3jqRtdLg1", "Lauryn Brough", 0, [1, 4]);
+User testUser = User(uid: "0000001", name: "Test", points: 0, favorites:[1, 4]);
